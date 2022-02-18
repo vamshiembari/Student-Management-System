b32a3db5dde74072b65d2931727029c52383302c
vamshiembari <vamshiembari12015@gmail.com>
2022-02-18 16:15:07 +0530

    adding files

---
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.sms;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public abstract class StudentManagementSystemApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+}
